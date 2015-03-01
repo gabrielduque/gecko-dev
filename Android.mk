e989582762e3affea854860581eba43c2af59ed8
GabrielDuque <gabrielferrazduque@gmail.com>
2015-03-01 12:01:52 -0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# License, v. 2.1. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.1/.
 
 # empty file to block B2G/Gonk from trying to build anything inside mozilla-central
